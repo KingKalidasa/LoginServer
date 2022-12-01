bb5a05b0e248cdbed842c08cbc0155deb2ce47f2
KingKalidasa <thelfq@gmail.com>
2022-12-01 09:30:32 -0600

    added some faux test code

---
+++ generate passwords.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const Cryptr = require('cryptr');
+const crypto = new Cryptr('hardcoded_key32');
+
+let pass1 = crypto.encrypt('welfj');
+let pass2 = crypto.encrypt('42454');
+let pass3 = crypto.encrypt('hunter2');
+
+let items = {
+	'usharold:': pass1,
+	'sggalvin:': pass2,
+	'dewendy:': pass3
+};
+console.log(items);
+jsonItems = JSON.stringify(items);
+
+fs.writeFile(
+	'secret passwords.txt',
+	jsonItems,
+	{
+		encoding: 'utf8',
+		flag: 'w',
+		mode: 0o666
+	},
+	(err) => {
+		if (err) console.log(err);
+		else {
+			console.log('File written successfully\n');
+			console.log('The written has the following contents:');
+			console.log(fs.readFileSync('secret passwords.txt', 'utf8'));
+		}
+	}
+);
